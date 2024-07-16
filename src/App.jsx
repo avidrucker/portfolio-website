2b47f8b99f93e7fb6bccf89d76733f58f3c643f4
avidrucker <avidrucker@gmail.com>
2024-07-16 18:47:37 -0400

    update add project names list

--- src/App.jsx
+++ src/App.jsx
@@ -31,13 +31,24 @@ function App() {
         </header>
           <section className="jumbotron tc">
             <h1>Software Expert</h1>
-            <h2>I help people solve real-world problems.</h2>
+            <h2>I help people solve real-world problems... for <strong>money</strong> 😉</h2>
           </section>
           <section className="projects">
-            <h2 className="w0 h0 o-0">Projects</h2>
+            {/* remove next h2 from screenreaders */}
+            <h2 className="w0 h0 o-0">End of Jumbotron</h2>
           </section>
-    
       </section>
+        <section className="second-page vh-100 flex flex-column justify-between">
+          <section className="projects">
+            <ul>
+              <li>Card Designer</li>
+              <li>AutoFocus</li>
+              <li>TreeSwipe</li>
+              <li>LCC Cloud</li>
+              <li>My Portfolio Site</li>
+            </ul>
+          </section>
+        </section>
       <footer>
         <nav className="flex items-center justify-between ph4 pv3">
           <p className="ma0 mr-auto">
