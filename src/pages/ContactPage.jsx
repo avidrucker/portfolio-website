f29a9990eb94d16f9778c1b95c4637503cf63841
Avi <avidrucker@gmail.com>
2025-12-18 10:49:46 -1000

    fix contact form to have email field, required fields

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,13 +1,15 @@
 function ContactPage() {
   return (
-    <div className="ph4 pv5 mw7 center">
-      <h1>Contact</h1>
-      <p>If you'd like to reach out, email <a href="mailto:hello@example.com">hello@example.com</a>.</p>
-      <form className="mt3">
+    <div className="ph4 pt5 pb3 mw7 center">
+      <h1 className="ma0 pt5 f2">Contact</h1>
+      <p className="measure ma0 pt2 f4">If you'd like to reach out, email avidrucker AT gmail DOT com or submit your name, email address, and a message below.</p>
+      <form className="pt3 pb5">
         <label className="db mb2">Name</label>
-        <input className="w-100 pa2 mb3" type="text" name="name" />
+        <input className="w-100 pa2 mb3" required type="text" name="name" placeholder="Homer Simpson" />
+        <label className="db mb2">Email</label>
+        <input className="w-100 pa2 mb3" required type="email" name="email" placeholder="hsimpson@gmail.com" />
         <label className="db mb2">Message</label>
-        <textarea className="w-100 pa2 mb3" name="message" rows="6" />
+        <textarea className="w-100 pa2 mb3" required name="message" rows="6" placeholder="Your message here..." />
         <button className="pa2" type="submit">Send</button>
       </form>
     </div>
