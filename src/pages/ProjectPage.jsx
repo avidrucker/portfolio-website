22479528d2cfc73d3a1a0b2d4278e7b917aa3da5
Avi <avidrucker@gmail.com>
2025-12-18 11:48:39 -1000

    update add project infos

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -4,25 +4,36 @@ const PROJECTS = {
   "card-designer": {
     title: "Card Designer",
     desc: "Digital flashcard editing enhanced",
+    github: "https://github.com/avidrucker/anki-card-test-1",
+    app: "https://avidrucker.github.io/anki-card-test-1/",
+    long: "I created this web app to simplify the process of designing and previewing digital flashcards for Anki. Built with React and Tachyons.",
   },
   autofocus: {
     title: "AutoFocus",
     desc: "Productivity via radical simplicity",
+    github: "https://github.com/avidrucker/pwa-autofocus-app",
+    app: "https://avidrucker.github.io/pwa-autofocus-app/",
+    long: "I developed this progressive web app to help people boost productivity through simplicity. It leverages the AutoFocus task management algorithm (originally designed by Mark Forster). Built with React and Tachyons CSS and designed for both desktop and mobile use.",
   },
   treeswipe: {
     title: "TreeSwipe",
     desc: "A Chrome extension for Gmail",
+    github: "https://github.com/avidrucker/tree-swipe/",
+    app: "https://chromewebstore.google.com/detail/tree-swipe/hlpelbabphidmofbjcijjjkagolaebjl?hl=en",
+    long: "I built TreeSwipe to enhance email management in Gmail by allowing users to quickly archive or delete emails with simple keyboard shortcuts. Developed as a Chrome extension using JavaScript and the Gmail API.",
   },
-  "lcc-cloud": {
-    title: "LCC Cloud",
-    desc: "Assembly in the cloud",
+  "lcc-js": {
+    title: "LCC.js",
+    desc: "Virtual assembler and interpreter",
+    github: "https://github.com/avidrucker/lccjs/",
+    long: "I built LCC.js to provide a console-based environment for assembling and interpreting LCC assembly language programs. Built with JavaScript and NodeJS.",
   },
   "web-map-app": {
     title: "Web Map App",
     desc: "Interactive mapping for everyone",
     github: "https://github.com/avidrucker/ship-log-map",
     app: "https://avidrucker.github.io/ship-log-map/",
-    long: "I built this PWA to enable people to create, edit, and share their own interactive maps with custom markers and data layers. Built with React, Cytoscape, and Tachyons.",
+    long: "I built this PWA to enable people to create, edit, and share their own interactive maps with custom markers and data layers. Built with React, Cytoscape, and Tachyons CSS.",
   },
 };
 
